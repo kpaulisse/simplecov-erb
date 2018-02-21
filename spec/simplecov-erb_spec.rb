a94c568d63237e30ae4308682a913dcbf2097313
Kevin Paulisse <kpaulisse@users.noreply.github.com>
2018-02-21 07:19:00 -0800

    Provide flexibility for setting environment variables

--- spec/simplecov-erb_spec.rb
+++ spec/simplecov-erb_spec.rb
@@ -7,27 +7,31 @@ require "shellwords"
 describe SimpleCov::Formatter::ERBFormatter do
   let(:subject) { described_class.new }
 
-  let(:file) { File.join(ENV["SIMPLECOV_ERB_TEMPDIR"], "coverage.txt") }
+  context "with a default configuration" do
+    before(:all) { generate_fixture }
 
-  describe "#format" do
-    let(:answer) do
-      <<-EOS
+    let(:file) { File.join(coveragedir, "coverage.txt") }
+
+    describe "#format" do
+      let(:answer) do
+        <<-EOS
 --------------------------------------------------------------------------------
 Incomplete test coverage
 --------------------------------------------------------------------------------
 
 /test/lib/baz.rb: 60.0% (missed: 4,8)
 /test/lib/foo.rb: 80.0% (missed: 8,16)
-      EOS
-    end
+        EOS
+      end
 
-    it "creates the coverage.txt file in the expected location" do
-      expect(File.file?(file)).to eq(true)
-    end
+      it "creates the coverage.txt file in the expected location" do
+        expect(File.file?(file)).to eq(true)
+      end
 
-    it "returns the correctly formatted message as per the default template" do
-      actual = File.read(file)
-      expect(actual).to eq(answer)
+      it "returns the correctly formatted message as per the default template" do
+        actual = File.read(file)
+        expect(actual).to eq(answer)
+      end
     end
   end
 end
